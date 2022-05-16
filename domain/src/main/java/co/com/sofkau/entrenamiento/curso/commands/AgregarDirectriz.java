97d7c04eb09f51a3fd8801d1edce2538e23aa85b
Aura Russill <auracris1996@hotmail.com>
2022-05-16 15:21:43 -0500

    ADD - se agrego AgregarDirectrizUseCase

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectriz.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectriz.java
@@ -1,14 +1,17 @@
 package co.com.sofkau.entrenamiento.curso.commands;
 
 import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
 
 public class AgregarDirectriz extends Command {
+    private final CursoId cursoId;
     private final MentoriaId mentoriaId;
     private final Directiz directiz;
 
-    public AgregarDirectriz(MentoriaId mentoriaId, Directiz directiz) {
+    public AgregarDirectriz(CursoId cursoId, MentoriaId mentoriaId, Directiz directiz) {
+        this.cursoId = cursoId;
         this.mentoriaId = mentoriaId;
         this.directiz = directiz;
     }
@@ -21,4 +24,6 @@ public class AgregarDirectriz extends Command {
         return directiz;
     }
 
+    public CursoId getCoursoId() { return cursoId;
+    }
 }
